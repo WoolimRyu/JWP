97e0a1e3310722a9ee7c32dc272bffb4077f17c3
heesu <heesu@nhnnext.org>
2015-02-28 00:39:16 +0900

    meet refactoring requirements
    1. indent depth should be less than 2
    2. do not use else
    3.method should work for only one purpose

--- StringCalculator/src/Calculator.java
+++ StringCalculator/src/Calculator.java
@@ -4,20 +4,23 @@ import java.util.regex.*;
 public class Calculator {
 	int add(String text) {
 		if(text.isEmpty()) return 0;
-		
+		String[] numbers = this.parse(text);
+		return this.sum(numbers);
+	}
+	
+	String[] parse(String text) {
 		String delimiter;
-		String[] numbers;
-		
 		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
 		if(m.find()) {
 			delimiter = m.group(1);
-			numbers = m.group(2).split(delimiter);
+			return m.group(2).split(delimiter);
 			
-		} else {
-			delimiter = ",|\n";
-			numbers = text.split(delimiter);
 		}
-		
+		delimiter = ",|\n";
+		return text.split(delimiter);
+	}
+	
+	int sum(String[] numbers) {
 		int result = 0;
 		for (int i = 0; i < numbers.length; i++) {
 			result += Integer.parseInt(numbers[i]);
