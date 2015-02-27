cea81702539aacff866b94a9cc23f84c9b67cd45
heesu <heesu@nhnnext.org>
2015-02-27 22:25:44 +0900

    deal with void string
    쪼갠길이 1, ParseInt()하면 NumFormatExpt.

--- StringCalculator/src/Calculator.java
+++ StringCalculator/src/Calculator.java
@@ -1,7 +1,9 @@
 
 public class Calculator {
 	int add(String text) {
+		if(text == "") return 0;
 		String[] tokens = text.split("[,\n]+");
+		
 		int result = 0;
 		for (int i = 0; i < tokens.length; i++) {
 			result += Integer.parseInt(tokens[i]);
