3289728bc4f17075503664251f9663bf01ee1f57
heesu <heesu@nhnnext.org>
2015-02-27 22:33:01 +0900

    implement parser and adder

--- StringCalculator/src/Calculator.java
+++ StringCalculator/src/Calculator.java
@@ -1,6 +1,11 @@
 
 public class Calculator {
 	int add(String text) {
-		return 0;
+		String[] tokens = text.split("[,\n]+");
+		int result = 0;
+		for (int i = 0; i < tokens.length; i++) {
+			result += Integer.parseInt(tokens[i]);
+		}
+		return result;
 	}
 }
