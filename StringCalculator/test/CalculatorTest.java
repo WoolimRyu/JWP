fe28e17edb99ce01ba05c00aab1795bb46c61e24
heesu <heesu@nhnnext.org>
2015-02-27 22:38:17 +0900

    remove repeated line

--- StringCalculator/test/CalculatorTest.java
+++ StringCalculator/test/CalculatorTest.java
@@ -4,29 +4,26 @@ import org.junit.Test;
 
 
 public class CalculatorTest {
-
+	Calculator c = new Calculator();
+	
 	@Test
 	public void voidStringShouldReturnZero() {
-		Calculator c = new Calculator();
 		assertEquals(c.add(""), 0);
 	}
 	
 	@Test
 	public void stringShouldBeSplitByComma() {
-		Calculator c = new Calculator();
 		assertEquals("1,2 must return 3", c.add("1,2"), 3);
 		assertEquals("1,2,3 must return 6", c.add("1,2,3"), 6);
 	}
 	
 	@Test
 	public void stringSholudBeSplitByNewline() {
-		Calculator c = new Calculator();
 		assertEquals(c.add("1\n2\n3"), 6);
 	}
 	
 	@Test
 	public void stringSholudBeSplitByCommaAndNewline() {
-		Calculator c = new Calculator();
 		assertEquals(c.add("1,2\n3"), 6);
 	}
 	
