c84888d89869d2dbe870092cfb2da429d5b167e5
heesu <heesu@nhnnext.org>
2015-02-27 22:04:53 +0900

    refactor test method name

--- StringCalculator/test/CalculatorTest.java
+++ StringCalculator/test/CalculatorTest.java
@@ -6,26 +6,26 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void testAssertVoidStringEqualsZero() {
+	public void voidStringShouldReturnZero() {
 		Calculator c = new Calculator();
 		assertEquals(c.add(""), 0);
 	}
 	
 	@Test
-	public void shouldBeSplitByComma() {
+	public void stringShouldBeSplitByComma() {
 		Calculator c = new Calculator();
 		assertEquals("1,2 must return 3", c.add("1,2"), 3);
 		assertEquals("1,2,3 must return 6", c.add("1,2,3"), 6);
 	}
 	
 	@Test
-	public void ShouldBeSplitByNewline() {
+	public void stringSholudBeSplitByNewline() {
 		Calculator c = new Calculator();
 		assertEquals(c.add("1\n2\n3"), 6);
 	}
 	
 	@Test
-	public void ShouldBeSplitByCommaAndNewline() {
+	public void stringSholudBeSplitByCommaAndNewline() {
 		Calculator c = new Calculator();
 		assertEquals(c.add("1,2\n3"), 6);
 	}
