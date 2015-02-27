fa6143c64f64e5eb161b45220c691b40890d9d6a
heesu <heesu@nhnnext.org>
2015-02-27 22:01:58 +0900

    test various string

--- StringCalculator/test/CalculatorTest.java
+++ StringCalculator/test/CalculatorTest.java
@@ -10,5 +10,24 @@ public class CalculatorTest {
 		Calculator c = new Calculator();
 		assertEquals(c.add(""), 0);
 	}
-
+	
+	@Test
+	public void shouldBeSplitByComma() {
+		Calculator c = new Calculator();
+		assertEquals("1,2 must return 3", c.add("1,2"), 3);
+		assertEquals("1,2,3 must return 6", c.add("1,2,3"), 6);
+	}
+	
+	@Test
+	public void ShouldBeSplitByNewline() {
+		Calculator c = new Calculator();
+		assertEquals(c.add("1\n2\n3"), 6);
+	}
+	
+	@Test
+	public void ShouldBeSplitByCommaAndNewline() {
+		Calculator c = new Calculator();
+		assertEquals(c.add("1,2\n3"), 6);
+	}
+	
 }
