2eb5671456fcb198c7b311edb556c019f99753b8
heesu <heesu@nhnnext.org>
2015-02-28 02:11:31 +0900

    create servlet class

---
+++ HelloWorld/src/net/heesu/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package net.heesu;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("Request Success!");
+		resp.getWriter().print("Hello World from Servlet");
+	}
+}
