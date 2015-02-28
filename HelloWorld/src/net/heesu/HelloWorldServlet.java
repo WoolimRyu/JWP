65e3e685be6956e2aadb389931bef4de294d4aa2
heesu <heesu@nhnnext.org>
2015-02-28 10:30:14 +0900

    use annotation instead of web.xml
    available w/ servlet 3.0~

--- HelloWorld/src/net/heesu/HelloWorldServlet.java
+++ HelloWorld/src/net/heesu/HelloWorldServlet.java
@@ -3,10 +3,13 @@ package net.heesu;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//@WebServlet("/helloworld")
+@WebServlet(name="HelloWorld", urlPatterns={"/helloworld", "/hello", "/hello/world"})
 public class HelloWorldServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
